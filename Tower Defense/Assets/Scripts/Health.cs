c6d833ca668da9face8261d2065cc5b39dd2014f
Narayan Headington <Narayan.Headington@scsstudent.org>
2026-05-27 14:14:29 -0700

    feat: Complete Part 11 and work on 11.5

---
+++ Tower Defense/Assets/Scripts/Health.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [field: SerializeField]
+    public float BaseHealth { get; private set; } = 2f;
+
+    [field: SerializeField]
+    public float Damage { get; private set; }
+
+}
---
+++ Tower Defense/Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [field: SerializeField]
+    public float Speed { get; private set; } = 2f;
+
+    [field: SerializeField]
+    public float Damage { get; private set; } = 1f;
+
+    [field: SerializeField]
+    public Transform Target {get; private set; }
+
+    void Update()
+    {
+        if (Target == null) {
+            return;
+        }
+    }
+}
